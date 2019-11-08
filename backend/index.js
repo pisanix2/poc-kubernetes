7a54f49e77bb885b51763fae626fa447640965bc
Pisani <rafael.pisani@totvs.com.br>
2019-11-08 09:00:50 -0300

    versao inicial do projeto

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.BACKEND_PORT || 8082
+
+app.use(bodyParser.json())
+
+app.post('/api/login', function (req, res) {
+  const { password } = req.body
+  if (password === 'Totvs@123') {
+    res.send({ logged: true })
+  } else {
+    res.status(500).send({ error: 'Dados do login incorreto' })
+  }
+})
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
+})
